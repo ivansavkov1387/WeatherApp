b4faf91c2b537c57f3ea52a454fc34337a4afbf5
Ivan Savkov <ivansavkov1387@gmail.com>
2021-11-17 21:20:38 +0300

    Start work

--- LoginFormController.swift
+++ LoginFormController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginFormController.swift
 //  WeatherApp
 //
 //  Created by Иван on 11/17/21.
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginFormController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
 
-
+    
 }
-
